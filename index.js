4899c6a3029312f9352003e989db38f93bea4544
morad <moradshapo@gmail.com>
2020-04-29 17:37:19 -0700

    mongogdb

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express =require("express")
+const express = require("express")
 const routers = require("./routes")
+const mongose = require("mongoose")
+const { db_url} = require("./config")
+
 const app=express()
+
+mongose.connect(db_url,function (err){
+    if (err) console.error(err)
+    console.log("database connected")
+})
 
 app.use(express.json())
 app.use(express.urlencoded())
+
 app.use(routers)
 
 app.listen(1249)
-console.log("app start at port 12")
+console.log("app start at port 1249")
--- routes.js
+++ routes.js
@@ -1,11 +1,22 @@
 const router =require ("express").Router()
-router.get("/books",function(req,res){
-    res.send("we are now in new module ")
+const mogoose =require("mongoose")
+const userschema = new mogoose.Schema({
+    username:{ type:String },
+    phone:{ type:Number },
+    email:{ type:String },
 })
-router.post("/books",function(req,res){
-    res.json({
-        msg:"you have aded new book",
-        data: req.body,
-    })
+const usermodel=mogoose.model("users",userschema)
+router.post("/addUser",function(req,res){
+    const data=req.body
+    new usermodel(data).save()
+    res.send("user added to database")
+})
+
+router.get("/getUser",function(req,res){
+   
+    const users=usermodel .find().exec()
+    users.then((data)=>res.json(data))
+    users.catch((err)=>res.json(err))
+    
 })
 module.exports=router
